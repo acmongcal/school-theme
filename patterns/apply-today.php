62db8c4262b619e8a5fc941b717efa5629238e61
Alan Christopher Mongcal <aranmongcal@gmail.com>
2026-09-16 20:29:55 -0700

    Added unsynced pattern

---
+++ patterns/apply-today.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Apply Today
+ * Slug: school-theme/apply-today
+ * Categories: media,text
+ */
+?>
+
+<!-- wp:cover {"url":"http://localhost:8888/school/wp-content/uploads/2026/08/learning-code-1024x683.jpg","id":15,"alt":"Team learning code together","dimRatio":80,"overlayColor":"primary","isUserOverlayColor":true,"sizeSlug":"large","align":"center","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover aligncenter"><img class="wp-block-cover__image-background wp-image-15 size-large" alt="Team learning code together" src="http://localhost:8888/school/wp-content/uploads/2026/08/learning-code-1024x683.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"placeholder":"Write title…","style":{"typography":{"textAlign":"center","fontSize":"8.9rem"}}} -->
+<p class="has-text-align-center" style="font-size:8.9rem">APPLY <br>TODAY</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
